9917ad0bb686a5723d65069a22592de0dcda2a18
Bespin03 <brianespimdola@gmail.com>
2025-05-18 01:24:57 -0600

    Create main entry point file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-center py-8">
+        Apptarea - Task Management Application
+      </h1>
+    </div>
+  )
+}
+
+export default App
